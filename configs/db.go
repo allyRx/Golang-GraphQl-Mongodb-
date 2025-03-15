d040ef377ccbaf5aa080fac319878a00684429cb
Herindrainyall2003 <herindrainyall2003@gmail.com>
2025-03-15 22:36:34 +0200

    add : create product

--- configs/db.go
+++ configs/db.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"project-mngt-golang-graphql/graph/model"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
@@ -20,17 +22,17 @@ type DB struct {
 //fonction qui occupe la connection entre la base des donnes
 func ConnectDB() *DB {
 	
-	client , err := mongo.NewClient(options.Client().ApplyURI(LoadEnvUrl())) //recupere le fonction helper du loading du .env
-
+	// Définir un contexte avec un timeout de 10 secondes
+	ctx , cancel := context.WithTimeout(context.Background() , 10*time.Second) 
+	defer cancel() // S'assurer que le contexte est annulé après usage
+	
+	// Connexion à MongoDB avec `mongo.Connect`
+	client , err := mongo.Connect(ctx , options.Client().ApplyURI(LoadEnvUrl())) //recupere la fonction helper du loading du .env
+	
 	if err != nil {
 		log.Fatal("Il s'agit l'erreur entre la connexion a la base de donnes peut etre le lien : ",err)
 	}
 
-	ctx , _ := context.WithTimeout(context.Background() , 10*time.Second) //initiate timeout 10seconds
-	err = client.Connect(ctx)
-	if err != nil { 
-		log.Fatal("Il s'agit d'une erreur au temps d'essaie de connexion normalement 10 secodes: " , err)
-	}
 
 	//ping the database
 	err = client.Ping(ctx , nil)
@@ -41,4 +43,35 @@ func ConnectDB() *DB {
 	fmt.Println("\u2707 Connected to mongodb")
 
 	return &DB{client: client}
+}
+
+//fonction pour acceder a notre base des donnes et retourne la reference a cette collection
+func colHelper(db *DB , collectionName string) *mongo.Collection{
+	return db.client.Database("ProjectManagement").Collection(collectionName)
+}
+
+//pour creer des projets
+func(db *DB) CreateProject(input *model.NewProject) (*model.Project , error) {
+	
+	//cette variable collection contient notre collection project
+	collection := colHelper(db , "Project")
+	ctx , cancel := context.WithTimeout(context.Background() , 10*time.Second)
+	defer cancel()
+
+
+	res , err := collection.InsertOne(ctx, input)
+
+	if err != nil {
+		return nil , err
+	}
+
+	project := &model.Project{
+		ID: 			res.InsertedID.(primitive.ObjectID).Hex(),
+		Owner: 			input.Owner,
+		Name: 			input.Name,
+		Description: 	input.Description,
+		Status: 		model.StatusNotStarted,	
+	}
+
+	return project , err
 }
